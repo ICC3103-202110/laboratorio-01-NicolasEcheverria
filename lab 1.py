3499d75a4401d73519ee555b959e97f04dae70ca
nicolas echeverria <necheverria@miuandes.cl>
2021-03-22 12:43:01 -0300

    lab 1 nicolas echeverria_0

---
+++ lab 1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 11:46:02 2021
+
+@author: nicolas
+"""
+
+lista_usuarios={}
+def usuarios_y_puntuaciones(lista):
+    print("escriba nombre de jugador")
+    nombre=input()
+    lista [nombre]=0
+    return lista
+
+tabla= usuarios_y_puntuaciones(lista_usuarios)
+
+print(tabla)
+
+
+
+
+
+
+
+
+
+
+
+
+
